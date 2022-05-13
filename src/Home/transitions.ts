3d40f85b71227c2426d9ad9396b2157d11d9cc4a
Ben Aepli <baepli@jhcschool.org>
2022-05-13 10:04:58 -0600

    fixed home transition

--- src/Home/transitions.ts
+++ src/Home/transitions.ts
@@ -45,17 +45,17 @@ export const createHeaderTransition = (state: boolean) => {
 export const createSubtitleTransition = (state: boolean, type: number) => {
   return {
     initial: {
-      y: "50vh",
+      y: "70vh",
     },
     animate: {
       y: "0",
       transition: {
-        duration: 0.7 - (type === 1 ? 0.1 : 0),
-        delay: 0.2 + (state ? INITIAL : LOADING) + (type === 1 ? 0.2 : 0),
+        duration: 0.8 - (type === 1 ? 0.1 : 0),
+        delay: 0.1 + (state ? INITIAL : LOADING) + (type === 1 ? 0.2 : 0),
       },
     },
     exit: {
-      y: "-50vh",
+      y: "-70vh",
     },
   };
 };
