fe34d849b46438c4c6aee5dfe5c43ffa9ec5a959
Ben Aepli <baepli@jhcschool.org>
2022-06-10 16:20:16 -0600

    changed styling

--- src/Admin/Edit/index.tsx
+++ src/Admin/Edit/index.tsx
@@ -40,6 +40,7 @@ export default function EditProject() {
         },
         body: JSON.stringify(Object.fromEntries(map)),
       });
+      window.location.reload();
     } catch (err) {
       return console.log(err);
     }
