0b450878d756cf1d5ba1afadad4b9617b94a9940
baepli-jhcs <90349602+baepli-jhcs@users.noreply.github.com>
2023-07-04 00:53:53 -0600

    Update lockfile and small format changes

--- src/About/index.tsx
+++ src/About/index.tsx
@@ -16,37 +16,43 @@ function About(props: { images: any }) {
       variants={pageTransition}
       className={AboutCSS.container}
     >
-      <div className={AboutCSS["image-container"]}>
-        <img className={AboutCSS.image} src={images.get("side")} alt="myself" />
-      </div>
-      <div className={AboutCSS.content}>
-        <div className={AboutCSS["content-container"]}>
-          <div className={AboutCSS.info}>
-            <div className={AboutCSS.title}> About Me </div>
-            <div className={AboutCSS.text}>
-              I am a senior at the Jackson Hole Community School, and I love
-              software development. These days, I primarily program graphics
-              applications with C++, and I plan to pursue this passion into
-              college and beyhond. For the past year, I've been working on a
-              project known as EXAGE (visible on the projects tab). I also have
-              some skill in web development through the MERN stack, and I am
-              plenty experienced with Python and Java.
-              <br />
-              <br />
-              On the side, I enjoy skiing, flying, and photography (the image to
-              your left is one of my photos of Wyoming wildlife!).
+      <div className={AboutCSS["inner-container"]}>
+        <div className={AboutCSS["image-container"]}>
+          <img
+            className={AboutCSS.image}
+            src={images.get("side")}
+            alt="myself"
+          />
+        </div>
+        <div className={AboutCSS.content}>
+          <div className={AboutCSS["content-container"]}>
+            <div className={AboutCSS.info}>
+              <div className={AboutCSS.title}> About Me </div>
+              <div className={AboutCSS.text}>
+                I am a senior at the Jackson Hole Community School, and I love
+                software development. These days, I primarily program graphics
+                applications with C++, and I plan to pursue this passion into
+                college and beyhond. For the past year, I've been working on a
+                project known as EXAGE (visible on the projects tab). I also
+                have some skill in web development through the MERN stack, and I
+                am plenty experienced with Python and Java.
+                <br />
+                <br />
+                On the side, I enjoy skiing, flying, and photography (the image
+                to your left is one of my photos of Wyoming wildlife!).
+              </div>
             </div>
-          </div>
-          <div className={AboutCSS.buttons}>
-            <Link
-              onClick={() => {
-                dispatch(loadActions.startLoading());
-              }}
-              to="/contact"
-              className={AboutCSS["contact-button"]}
-            >
-              Contact Me
-            </Link>
+            <div className={AboutCSS.buttons}>
+              <Link
+                onClick={() => {
+                  dispatch(loadActions.startLoading());
+                }}
+                to="/contact"
+                className={AboutCSS["contact-button"]}
+              >
+                Contact Me
+              </Link>
+            </div>
           </div>
         </div>
       </div>
